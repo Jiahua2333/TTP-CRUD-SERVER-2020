1ff9752a07ce9e6a413725265da5a011337158c8
Jiahua Liao <ljh15088131916@gmail.com>
2020-06-12 16:03:39 -0400

    update

--- utils/createLocalDatabase.js
+++ utils/createLocalDatabase.js
@@ -1,13 +1,13 @@
 const pgtools = require("pgtools");
 
 // An object with user, host, port, and password properties;
-const config = process.env.DATABASE_URL || {
-  user: "postgres",
+const config =  {
+  user: "ljh15",
   host: "localhost",
   port: 5432,
   password: process.env.LOCAL_DATABASE_PASSWORD,
 };
-// const config = process.env.DATABASE_URL;
+// const config = 'postgres://postgres:3112001944ljh@localhost/postgres';
 
 // The name of the database to create;
 const databaseName = require("./databaseName");
@@ -27,5 +27,5 @@ const cb = (err, res) => {
 };
 
 const createLocalDatabase = () => pgtools.createdb(config, databaseName, cb);
-
+console.log(config, databaseName);
 module.exports = createLocalDatabase;
